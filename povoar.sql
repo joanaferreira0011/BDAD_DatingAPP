805a9b6d855353855919abfbd6b56f81a1364b5b
joanaferreira0011@gmail.com <joanaferreira0011@gmail.com>
2019-04-14 21:36:31 +0100

    changed date format

--- povoar.sql
+++ povoar.sql
@@ -12,9 +12,9 @@ INSERT INTO User(idUser, userName, primeiroNome, ultimoNome, genero, password, e
 INSERT INTO User(idUser, userName, primeiroNome, ultimoNome, genero, password, email, dataNascimento, interesses) VALUES (8, "jinping", "Xi", "Jinping", "Man", "presidentForever", "xijinping@china.com", "15/06/1953","Communism");
 
 --ContaPremium
-INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (0, "01/02/2015", "10/06/2018", 0); -- Willy Wonka
-INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (2, "01/03/2010", "10/06/2018", -480); -- Donald Trump
-INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (2, "01/02/2017", "10/06/2018", -10); -- Theresa May
+INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (0, "2015-02-01", "2016-10-06", 0); -- Willy Wonka
+INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (2, "2010-03-01", "2018-10-06", -480); -- Donald Trump
+INSERT INTO ContaPremium(idUser, dataPagamentoInicio, dataPagamentoProximo, saldo) VALUES (2, "2017-02-01", "2018-10-06", -10); -- Theresa May
 
 --ContaFree
 INSERT INTO ContaFree(idUser, mesGratisUsado) VALUES (1, 0); -- Bernie Sanders
@@ -48,14 +48,14 @@ INSERT INTO AnuncioTemplate(idAnuncioTemplate, click, tipoAnuncio) VALUES(3, 0, 
 
 
 --Like
-INSERT INTO Like(idLike, user1, user2, data) VALUES (1, 2,7,"01/03/2016"); -- Trump to Putin
-INSERT INTO Like(idLike, user1, user2, data) VALUES (2, 2,4,"02/03/2018"); -- Trump to May
-INSERT INTO Like(idLike, user1, user2, data) VALUES (3, 2,5,"04/03/2018"); -- Trump to Kim Jong-un
-INSERT INTO Like(idLike, user1, user2, data) VALUES (4, 5,2,"05/03/2018"); -- Kim Jong-Un to Trump
-INSERT INTO Like(idLike, user1, user2, data) VALUES (5, 3,0,"05/03/2019"); -- Hillary to Wonka
-INSERT INTO Like(idLike, user1, user2, data) VALUES (6, 5,8,"07/04/2018"); -- Kim Jong-Un to Xi Jinping
-INSERT INTO Like(idLike, user1, user2, data) VALUES (7, 3,1,"05/03/2016"); -- Hillary to Bernie
-INSERT INTO Like(idLike, user1, user2, data) VALUES (8, 8,5,"08/04/2018"); -- Xi Jinping to Kim Jong-un
+INSERT INTO Like(idLike, user1, user2, data) VALUES (1, 2,7,"2016-03-01"); -- Trump to Putin
+INSERT INTO Like(idLike, user1, user2, data) VALUES (2, 2,4,"2018-03-02"); -- Trump to May
+INSERT INTO Like(idLike, user1, user2, data) VALUES (3, 2,5,"2018-03-04"); -- Trump to Kim Jong-un
+INSERT INTO Like(idLike, user1, user2, data) VALUES (4, 5,2,"2018-03-05"); -- Kim Jong-Un to Trump
+INSERT INTO Like(idLike, user1, user2, data) VALUES (5, 3,0,"2019-03-05"); -- Hillary to Wonka
+INSERT INTO Like(idLike, user1, user2, data) VALUES (6, 5,8,"2018-04-07"); -- Kim Jong-Un to Xi Jinping
+INSERT INTO Like(idLike, user1, user2, data) VALUES (7, 3,1,"2016-03-05"); -- Hillary to Bernie
+INSERT INTO Like(idLike, user1, user2, data) VALUES (8, 8,5,"2018-04-08"); -- Xi Jinping to Kim Jong-un
 
 --Match
 INSERT INTO Match(idMatch, like1, like2) VALUES (1, 3,4); -- Trump and Kim Jong-un
@@ -67,20 +67,19 @@ INSERT INTO Local(idLocal, morada, pais) VALUES (1, 'Capella Hotel Singapore','S
 INSERT INTO Local(idLocal, morada, pais) VALUES (2, 'Sofitel Legend Metropole Hanoi','Vietnam');
 INSERT INTO Local(idLocal, morada, pais) VALUES (3, 'Zhongnanhai','China');
 
-
 --Encontro
-INSERT INTO Encontro(idEncontro, match, local, data) VALUES (1, 1, 1, '12/06/2018'); -- Trump and Kim 1
-INSERT INTO Encontro(idEncontro, match, local, data) VALUES (2, 1, 2, '27/02/2019'); -- Trump and Kim 2
-INSERT INTO Encontro(idEncontro, match, local, data) VALUES (3, 2, 3, '25/03/2018'); -- Xi and Kim 1
+INSERT INTO Encontro(idEncontro, match, local, data) VALUES (1, 1, 1, '2018-06-12'); -- Trump and Kim 1
+INSERT INTO Encontro(idEncontro, match, local, data) VALUES (2, 1, 2, '2019-02-27'); -- Trump and Kim 2
+INSERT INTO Encontro(idEncontro, match, local, data) VALUES (3, 2, 3, '2018-03-25'); -- Xi and Kim 1
 
 --Mensagem
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (1, 1, 2, 'Hey Kim, I think we gotta talk', '06/03/2018'); -- Trump to Kim
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (2, 1, 5, 'Hey Trump, sure. Come to Peyongyang.', '06/03/2018');
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (3, 1, 2, 'Come to Washington.', '07/03/2018');
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (4, 1, 5, 'Aint got a plane. Lets meet in the middle', '07/03/2018');
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (5, 1, 2, 'Vietnam it is.', '07/03/2018');
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (6, 2, 5, 'Xi, you there? I just mathed with Trump, need advice.', '06/03/2018'); -- Kim to Xi
-INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (7, 2, 8, 'Sure, come meet me', '07/03/2018'); -- Xi to Kim
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (1, 1, 2, 'Hey Kim, I think we gotta talk', '2018-03-06'); -- Trump to Kim
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (2, 1, 5, 'Hey Trump, sure. Come to Peyongyang.', '2018-03-06');
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (3, 1, 2, 'Come to Washington.', '2018-03-07');
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (4, 1, 5, 'Aint got a plane. Lets meet in the middle', '2018-03-07');
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (5, 1, 2, 'Vietnam it is.', '2018-03-07');
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (6, 2, 5, 'Xi, you there? I just mathed with Trump, need advice.', '2018-03-06'); -- Kim to Xi
+INSERT INTO Mensagem(idMensagem, match, remetente, conteudo, data) VALUES (7, 2, 8, 'Sure, come meet me', '2018-03-07'); -- Xi to Kim
 
 --Denuncia
-INSERT INTO Denuncia(idDenuncia, mensagem, descricao, data) VALUES (1, 4, 'I though this app selected by money', '07/03/2018');
+INSERT INTO Denuncia(idDenuncia, mensagem, descricao, data) VALUES (1, 4, 'I though this app selected by money', '2018-03-07');

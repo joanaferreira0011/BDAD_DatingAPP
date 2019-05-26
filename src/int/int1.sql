ef86dd325cd4d4e9fb6f872328c7b82913b9f93f
joanaferreira0011 <joanaferreira0011@gmail.com>
2019-05-26 17:49:29 +0100

    started view for match

--- src/int/int1.sql
+++ src/int/int1.sql
@@ -2,9 +2,21 @@
 .headers on
 .nullvalue NULL
 
--- SELECT * 
+DROP VIEW IF EXISTS usersHobby;
+-- SELECT *
 -- FROM User CROSS JOIN ContaPremium;
 
 -- Select User, (Count(ContaPremium.idUser)* 100 / (Select Count(*) From MyTable)) as Score
 
 -- count(ContaPremium.idUser)/count(User.idUser)
+
+CREATE VIEW usersHobby
+as
+select userName,idUser, carros, tecnologia, comida
+from User, Hobby
+where User.idHobby=Hobby.idHobby;
+
+
+select *
+from usersHobby target, usersHobby others
+where target.idUser=0 and target.idUser<> others.idUser;

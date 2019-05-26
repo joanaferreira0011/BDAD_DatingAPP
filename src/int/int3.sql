44ba158878b5d348fba34ff407593e110120af57
joanaferreira0011 <joanaferreira0011@gmail.com>
2019-05-26 23:51:30 +0100

    fixed

--- src/int/int3.sql
+++ src/int/int3.sql
@@ -2,10 +2,11 @@
 .headers on
 .nullvalue NULL
 
-
-select Anunciante.nome, Campanha.conteudo, 1.0*sum(AnuncioTemplate.click)/count(AnuncioTemplate.click) as acceptance
-from TipoAnuncio, AnuncioTemplate, Campanha, Anunciante
+Select Anunciante.nome,c, max(acceptance) as aceitacao
+from Anunciante, (
+select Campanha.idAnunciante as a, Campanha.conteudo as c, 1.0*sum(AnuncioTemplate.click)/count(AnuncioTemplate.click) as acceptance
+from TipoAnuncio, AnuncioTemplate, Campanha
 where AnuncioTemplate.tipoAnuncio=TipoAnuncio.idTipoAnuncio
       and TipoAnuncio.campanha=Campanha.idCampanha
-      and Campanha.idAnunciante=Anunciante.idAnunciante
-group by Anunciante.idAnunciante;
+group by Campanha.idCampanha) as campanhas
+where campanhas.a = Anunciante.idAnunciante;

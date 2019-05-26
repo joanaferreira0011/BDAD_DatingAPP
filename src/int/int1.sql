8fc1fa0a86bdb69428fc6af1cd9233918e53997f
joanaferreira0011 <joanaferreira0011@gmail.com>
2019-05-26 22:59:05 +0100

    fixed minor error

--- src/int/int1.sql
+++ src/int/int1.sql
@@ -13,7 +13,7 @@ where User.idHobby=Hobby.idHobby;
 
 CREATE VIEW possibleMatch
 as
-SELECT targetName, otherName,
+SELECT targetName, otherName, c, t, co,
   (carros+tecnologia+comida)/2 as intercessao,
   c+t+co as reuniao
   from(
@@ -21,15 +21,15 @@ select target.userName as targetName, other.userName as otherName,
        target.carros+ other.carros as carros,
        target.tecnologia+ other.tecnologia as tecnologia,
        target.comida+ other.comida as comida,
-       (target.carros+1)/2 as c,
-       (target.tecnologia+1)/2 as t,
-       (target.comida+1)/2 as co
+       (target.carros+ other.carros+1)/2 as c,
+       (target.tecnologia+ other.tecnologia +1)/2 as t,
+       (target.comida+ other.carros+1)/2 as co
 from usersHobby target, usersHobby other
 where target.idUser=0 and target.idUser<> other.idUser);
 
-SELECT targetName, otherName, 1.0*intercessao/reuniao as jaccard
-from possibleMatch;
-
+SELECT targetName, otherName,1.0*intercessao/reuniao as jaccard
+from possibleMatch
+ORDER BY(jaccard) DESC;
 
 
 

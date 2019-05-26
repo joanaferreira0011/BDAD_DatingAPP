ce5325e6f105d7e1a1605527ce1bb788e9319acf
joanaferreira0011 <joanaferreira0011@gmail.com>
2019-05-26 20:41:22 +0100

    changed int2

--- src/int/int2.sql
+++ src/int/int2.sql
@@ -2,20 +2,13 @@
 .headers on
 .nullvalue NULL
 
--- SELECT SUM(strftime('%d',Encontro.match)-
--- strftime('%d',data.encontro)) "No de dias"
--- FROM Match, Encontro
--- WHERE Match.idMatch=Encontro.match
--- GROUP BY idMatch;
 
-SELECT idMatch, user1.userName, user2.userName, max(dif)
-from User user1,User user2, (
+SELECT avg(dif) as average
+from (
 
       SELECT idMatch, like1.user1 as user1, like1.user2 as user2, strftime('%d',Encontro.data)- strftime('%d',like1.data) as dif
       FROM Match, Encontro, Like like1, Like like2
       WHERE Match.idMatch=Encontro.match
             and Match.like1=like1.idLike
             and Match.like2=like2.idLike
-      GROUP BY idMatch) as likes
-
-where user1.idUser = likes.user1 and user2.idUser= likes.user2;
+      GROUP BY idMatch) as likes;
